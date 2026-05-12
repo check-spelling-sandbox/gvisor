2a936ee42d532a400023d5ebc84478f6bf190b03
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-12 19:47:05 -0400

    spelling: link-local
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/tcpip/network/ipv6/ndp.go
+++ pkg/tcpip/network/ipv6/ndp.go
@@ -804,7 +804,7 @@ func (ndp *ndpState) handleRA(ip tcpip.Address, ra header.NDPRouterAdvert) {
 		case header.NDPPrefixInformation:
 			prefix := opt.Subnet()
 
-			// Is the prefix a link-local?
+			// Is the prefix link-local?
 			if header.IsV6LinkLocalUnicastAddress(prefix.ID()) {
 				// ...Yes, skip as per RFC 4861 section 6.3.4,
 				// and RFC 4862 section 5.5.3.b (for SLAAC).
