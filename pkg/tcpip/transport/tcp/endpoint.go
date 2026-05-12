25256b413aca520e778e239b017fc3e9ed28df88
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-12 19:45:00 -0400

    spelling: directly invoke
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/tcpip/transport/tcp/endpoint.go
+++ pkg/tcpip/transport/tcp/endpoint.go
@@ -2875,7 +2875,7 @@ func (e *Endpoint) getRemoteAddress() tcpip.FullAddress {
 func (*Endpoint) HandlePacket(stack.TransportEndpointID, *stack.PacketBuffer) {
 	// TCP HandlePacket is not required anymore as inbound packets first
 	// land at the Dispatcher which then can either deliver using the
-	// worker go routine or directly do the invoke the tcp processing inline
+	// worker go routine or directly invoke the tcp processing inline
 	// based on the state of the endpoint.
 }
 
