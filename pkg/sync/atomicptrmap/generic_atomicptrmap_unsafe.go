faa9dff23f579ed40dac7583a7215fc8258a8317
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-13 14:06:37 -0400

    spelling: rehashes
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/sync/atomicptrmap/generic_atomicptrmap_unsafe.go
+++ pkg/sync/atomicptrmap/generic_atomicptrmap_unsafe.go
@@ -136,7 +136,7 @@ const (
 	apmShardLookupDataPadding           = apmEnablePadding * apmShardLookupDataRequiredPadding
 
 	// These define fractional thresholds for when apmShard.rehash() is called
-	// (i.e. the load factor) and when it rehases to a larger table
+	// (i.e. the load factor) and when it rehashes to a larger table
 	// respectively. They are chosen such that the rehash threshold = the
 	// expansion threshold + 1/2, so that when reuse of deleted slots is rare
 	// or nonexistent, rehashing occurs after the insertion of at least 1/2
