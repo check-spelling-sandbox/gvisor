2cb55c3d9a94741fa811714c2c6395d4f60a2533
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-13 10:32:43 -0400

    spelling: semaphores
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/syscalls/linux/semaphore.cc
+++ test/syscalls/linux/semaphore.cc
@@ -1013,7 +1013,7 @@ TEST(SemaphoreTest, SemInfo) {
   EXPECT_EQ(info.semmsl, kSemMsl);
   EXPECT_EQ(info.semopm, kSemOpm);
   EXPECT_EQ(info.semume, kSemUme);
-  // Apart from semapahores that are not created by the test, we can't determine
+  // Apart from semaphores that are not created by the test, we can't determine
   // the exact number of semaphore sets and semaphores, as a result, semusz and
   // semaem range from 0 to a random number. Since the numbers are always
   // non-negative, the test will not check the results of semusz and semaem.
