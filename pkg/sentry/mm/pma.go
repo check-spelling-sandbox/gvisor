ab920b7b238b9a69c958657e14e1abe7b881e500
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-12 20:21:38 -0400

    spelling: equals
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/sentry/mm/pma.go
+++ pkg/sentry/mm/pma.go
@@ -200,7 +200,7 @@ func (mm *MemoryManager) getDefaultAllocationDirection() pgalloc.Direction {
 func (mm *MemoryManager) getAllocationDirection(ar hostarch.AddrRange, vma *vma) pgalloc.Direction {
 	lastFault := atomic.LoadUintptr(&vma.lastFault)
 	arStart := uintptr(ar.Start)
-	// If this VMA does not have last page fault or last page fault equals to arStart,
+	// If this VMA does not have last page fault or last page fault equals arStart,
 	// use the default allocation direction.
 	if lastFault == 0 || lastFault == arStart {
 		return mm.getDefaultAllocationDirection()
