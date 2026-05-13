ca25bc81b81fd08d2ffcdbaae1f89a51ec4a39d5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-12 20:15:13 -0400

    spelling: to
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/tcpip/stack/registration.go
+++ pkg/tcpip/stack/registration.go
@@ -691,7 +691,7 @@ const (
 	PermanentExpired
 
 	// Temporary is an endpoint, created on a one-off basis to temporarily
-	// consider the NIC bound an an address that it is not explicitly bound to
+	// consider the NIC bound to an address that it is not explicitly bound to
 	// (such as a permanent address). Its reference count must not be biased by 1
 	// so that the address is removed immediately when references to it are no
 	// longer held.
