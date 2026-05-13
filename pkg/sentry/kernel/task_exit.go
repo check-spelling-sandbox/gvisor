623b31f2a67926a1f49582ea3ca0057074c1d019
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-13 01:59:06 -0400

    spelling: children's
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/sentry/kernel/task_exit.go
+++ pkg/sentry/kernel/task_exit.go
@@ -1215,7 +1215,7 @@ func (t *Task) waitCollectZombieLocked(target *Task, opts *WaitOptions, asPtrace
 			t.tg.childCPUStats.Accumulate(target.tg.CPUStats())
 			t.tg.childCPUStats.Accumulate(target.tg.childCPUStats)
 			// Update t's child max resident set size. The size will be the maximum
-			// of this thread's size and all its childrens' sizes.
+			// of this thread's size and all its children's sizes.
 			if t.tg.childMaxRSS < target.tg.maxRSS {
 				t.tg.childMaxRSS = target.tg.maxRSS
 			}
