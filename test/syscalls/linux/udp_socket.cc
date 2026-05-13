e89248b624af0a4dfeace60d4e6c0895aee881ed
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-13 02:02:30 -0400

    spelling: concurrently
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/syscalls/linux/udp_socket.cc
+++ test/syscalls/linux/udp_socket.cc
@@ -796,7 +796,7 @@ TEST_P(UdpSocketTest, ConnectAndSendNoReceiver) {
   ASSERT_NO_ERRNO(BindLoopback());
   // Connect to loopback:bind_addr_ which should *hopefully* not be bound by an
   // UDP socket. There is no easy way to ensure that the UDP port is not bound
-  // by another conncurrently running test. *This is potentially flaky*.
+  // by another concurrently running test. *This is potentially flaky*.
   ASSERT_THAT(connect(sock_.get(), bind_addr_, addrlen_), SyscallSucceeds());
 
   // Close the socket after connecting to the bound address to make sure `sock_`
@@ -903,7 +903,7 @@ TEST_P(UdpSocketTest, RecvErrorConnRefused) {
 
   // Connect to loopback:bind_addr_ which should *hopefully* not be bound by an
   // UDP socket. There is no easy way to ensure that the UDP port is not bound
-  // by another conncurrently running test. *This is potentially flaky*.
+  // by another concurrently running test. *This is potentially flaky*.
   const int kBufLen = 300;
   ASSERT_THAT(connect(sock_.get(), bind_addr_, addrlen_), SyscallSucceeds());
   char buf[kBufLen];
